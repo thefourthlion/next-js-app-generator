f59be798190d844bbf0c5379880b705175be0469
TheFourthLion <46293419+thefourthlion@users.noreply.github.com>
2022-11-07 06:26:04 -0800

    Update generate-page.py

--- generate-page.py
+++ generate-page.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 
 def writeToFile(path, content):
@@ -5,103 +7,52 @@ def writeToFile(path, content):
   file.write(content)
   file.close()
 
-start_num = 0
-file_num = 0
-
 # ----------------------------------------- Set according to directory - var ----------------------------
-# ****** REPLACE THIS WITH PROJECT DIRECTORY *********
-current_path = (r"Z:\programming\next-template")
+current_path = (r"Z:\programming\generate-next-code")
 
-component_directory = "components"
-page_directory = "pages"
-styles_directory = "styles"
+component_folder = "components"
+page_folder = "pages"
+styles_folder = "styles"
+public_folder = "public"
 
-component_path = os.path.join(current_path, component_directory)
-page_path = os.path.join(current_path, page_directory)
-styles = os.path.join(current_path, styles_directory)
-
-def react_content_func(name):
-  intro = "import React from 'react'; const "
-  mid = "= () => { return ( <div className='"
-  end="'>name</div>);}; export default "
-  react_content= intro + name + mid + name + end + name
-  return react_content
-
-def component_content_func(name):
-  intro = "import React from 'react'; const "
-  mid = "= (props) => { return ( <div className='"
-  end="'>name</div>);}; export default "
-  react_content= intro + name + mid + name + end + name
-  return react_content
-
-def style_content_func(name):
-  style = "." + name + "{}"
-  return style 
-
-styles_content = ".Example{}"
-component_content="import React from 'react'; const Example = () => { return ( <div className='Example'>Example</div>);};"
-
-js_component_path = './components/'
-js_page_path = './pages/'
-styles_path = "./styles/"
-
-user_page_input = "y"
-
-# ----------------------------------------- Create component directory ----------------------------
-print("Components path being made...")
-if os.path.exists(component_path):
-  print("Component path already exists.")
-else:
-  os.mkdir(component_path)
-
-# ----------------------------------------- Create pages directory ----------------------------
-print("Page path being made...")
-if os.path.exists(page_path):
-  print("Page path already exists.")
-else:
-  os.mkdir(page_path)
-
-# ----------------------------------------- Create styles directory ----------------------------
-print("Style path being made...")
-if os.path.exists(styles_path):
-  print("Style path already exists.")
-else:
-  os.mkdir(styles_path)
-
-# ----------------------------------------- Create pages and styles ----------------------------
-while user_page_input.lower() == "y" or user_page_input.lower() == "yes":
-  user_page_input = input("Would you like to add a page?")
-  if user_page_input.lower() == "y" or user_page_input.lower() == "yes":
-    page_name = input("What is the page name?")
-    print(f"Adding page and css for {page_name}...")
-    page_file_dir = js_page_path + page_name + ".js"
-    page_style_dir = styles_path + page_name + ".scss"
-    styles_content = f".{page_name}" + "{}"
-    writeToFile(page_file_dir, react_content_func(page_name))
-    writeToFile(page_style_dir, style_content_func(page_name))
-    styles = "import '../styles/" + page_name + ".scss'"
-    with open('./pages/_app.js', 'r+') as file: 
-        file_data = file.read() 
-        file.seek(0, 0) 
-        file.write(styles + '\n' + file_data) 
-
-# ----------------------------------------- Create components and styles ----------------------------
-user_component_input = "y"
-
-while user_component_input.lower() == "y" or user_component_input.lower() == "yes":
-  user_component_input = input("Would you like to add a component?")
-  if user_component_input.lower() == "y" or user_component_input.lower() == "yes":
-    component_name = input("What is the components name?")
-    print(f"Adding page and css for {component_name}...")
-    component_file_dir = js_component_path + component_name + ".js"
-    component_style_dir = styles_path + page_name + ".scss"
-    styles_content = f".{page_name}" + "{}"
-    writeToFile(component_file_dir, component_content_func(page_name))
-    writeToFile(component_style_dir, style_content_func(page_name))
-    styles = "import '../styles/" + page_name + ".scss'"
-    with open('./pages/_app.js', 'r+') as file: 
-        file_data = file.read() 
-        file.seek(0, 0) 
-        file.write(styles + '\n' + file_data) 
+component_directory = os.path.join(current_path, component_folder)
+page_directory = os.path.join(current_path, page_folder)
+styles_directory = os.path.join(current_path, styles_folder)
+public_directory = os.path.join(current_path, public_folder)
 
 
+  
+def create_components(page_name, component_name):
+  component_file_path = f"{component_directory}/{page_name}/{component_name}.js"
+  component_content = 'import React from "react"; const ' + component_name + ' = () => { return <div className=" ' + component_name  + '">' + component_name  + '</div>; }; '
+  if os.path.exists(component_file_path):
+    print("This component already exists")
+  else:
+    os.mkdir(f"{component_directory}/{page_name}")
+    writeToFile(component_file_path, component_content)
+    
+def create_pages( page_name):
+  page_file_path = f"{page_directory}/{page_name}.js"
+  page_content = 'import React from "react"; const ' + page_name + ' = () => { return <div className=" ' + page_name  + '">' + page_name  + '</div>; }; '
+  if os.path.exists(page_file_path):
+    print("This page already exists")
+  else:
+    if os.path.exists(page_directory):
+      print("This page already exists")
+    else:
+      os.mkdir(page_directory)
+    writeToFile(page_file_path, page_content)
+
+
+# create_components("Home", "HomePreview")
+# create_pages("Home")
+
+
+# components folder, file, and Style
+# pages folder file, and style
+# if package json isn't there, create it with next - react - react dom - next 
+# make a public folder and images folder if not there 
+# make globals css if not there
+# make app and index if not there
+# everytime style is added, import it to app
+# use page name, for components folder
